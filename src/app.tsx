d2836940bbb09c01e6cce97e4646d326fed81861
Daniel Júnior <daniel.junior.ifpb@gmail.com>
2024-02-06 18:03:07 -0300

    feat: first commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import logo from './assets/logo-nlw-expert.svg';
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12">
+      <img src={logo} alt="NLW Expert" />
+      <form>
+        <input type="text" placeholder="Busque em suas notas..." />
+      </form>
+    </div>
+  );
+}
